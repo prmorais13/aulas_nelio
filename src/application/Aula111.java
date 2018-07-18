d7613a839b3fa831eb51f583084d2d8edd5f944e
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-07-18 11:58:40 -0300

    Segundo commit

---
+++ src/application/Aula111.java
@@ -0,0 +1,10 @@
+package application;
+
+public class Aula111 {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
---
+++ src/entities/models/aula111/Reservas.java
@@ -0,0 +1,60 @@
+package entities.models.aula111;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservas {
+
+	private Integer numQuarto;
+	private Date entrada;
+	private Date saida;
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Reservas(Integer numQuarto, Date entrada, Date saida) {
+		this.numQuarto = numQuarto;
+		this.entrada = entrada;
+		this.saida = saida;
+	}
+
+	public Integer getNumQuarto() {
+		return numQuarto;
+	}
+
+	public void setNumQuarto(Integer numQuarto) {
+		this.numQuarto = numQuarto;
+	}
+
+	public Date getEntrada() {
+		return entrada;
+	}
+
+	public Date getSaida() {
+		return saida;
+	}
+	
+	public long duracao() {
+		long diff = this.saida.getTime() - this.entrada.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+	
+	public void atualizaDatas(Date entrada, Date saida) {
+		this.entrada = entrada;
+		this.saida = saida;
+	}
+	
+	@Override
+	public String toString() {
+		return "Quarto "
+				+ this.numQuarto
+				+ ", Entrada: "
+				+ sdf.format(this.entrada)
+				+ ", Saída: "
+				+ sdf.format(this.saida)
+				+ ", "
+				+ this.duracao()
+				+ " Noites";
+	}
+	
+}
