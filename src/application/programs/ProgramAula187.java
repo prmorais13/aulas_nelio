d9b5d42cc7716d75fde0f16538a504a71b6c5627
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-08-02 23:03:32 -0300

    Aula 188 - Concluida

--- src/application/programs/ProgramAula187.java
+++ src/application/programs/ProgramAula187.java
@@ -2,6 +2,8 @@ package application.programs;
 
 import java.util.ArrayList;
 import java.util.List;
+//import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 import model.aula180.entities.Produto;
 
@@ -15,9 +17,7 @@ public class ProgramAula187 {
 		list.add(new Produto("Tablet", 350.50));
 		list.add(new Produto("HD Case", 80.90));
 		
-		double min = 100.00;
-		
-		list.forEach(p -> p.setPreco(p.getPreco() * 1.1));
+		// double min = 100.00;
 		
 		/*list.removeIf(p -> p.getPreco() >= min);
 		
@@ -25,7 +25,21 @@ public class ProgramAula187 {
 			System.out.println(produto);
 		}*/
 		
-		list.forEach(System.out :: println);
+/*		double factor = 1.1;
+		
+		Consumer<Produto> cons = p -> {
+			p.setPreco(p.getPreco() * factor);
+		};
+		
+		list.forEach(cons);*/
+		
+		//list.forEach(p -> p.setPreco(p.getPreco() * 1.1));
+		
+		List<String> nomes = list.stream().map(p -> p.getNome().toUpperCase()).collect(Collectors.toList());
+		
+		nomes.forEach(System.out :: println);
+		
+		//list.forEach(System.out :: println);
 
 	}
 
